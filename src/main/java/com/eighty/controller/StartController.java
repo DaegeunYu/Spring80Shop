b49d8b68aeb4c80adc2c46413e824a52993ca1ca
DaegeunYu <73866484+DaegeunYu@users.noreply.github.com>
2026-01-23 12:24:20 +0900

    Initial commit

---
+++ src/main/java/com/eighty/controller/StartController.java
@@ -0,0 +1,19 @@
+package com.eighty.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+	StartController(){
+		System.out.println("==> StartController");
+	}
+	
+	@GetMapping(value="/index.do")
+	public String index(HttpServletRequest request, HttpSession sesstion) {
+		return "index.jsp";
+	}
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<c:import url="/WEB-INF/view/include/top.jsp" />
+<section>
+<br>
+<div align="center">
+<h1> Spring MVC </h1>
+
+<a href=index.do> index 확인 </a>
+
+</div>
+<br>
+</section>
+<c:import url="/WEB-INF/view/include/bottom.jsp" />
